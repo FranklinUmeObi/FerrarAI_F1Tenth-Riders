efb10a85c0c83a965889dca4fe64a72d5c33419a
Chris Staunton <stauntch@tcd.ie>
2021-06-03 17:09:30 +0100

    added chris driver

--- f1tenth-riders-quickstart/pkg/src/pkg/drivers.py
+++ f1tenth-riders-quickstart/pkg/src/pkg/drivers.py
@@ -86,10 +86,179 @@ class Sam:
 
 
 class Chris:
+    
+    CAR_WIDTH = 0.31
+    # the min difference between adjacent LiDAR points for us to call them disparate
+    DIFFERENCE_THRESHOLD = 2.
+    SPEED = 12. 
+    MAX_SPEED = 12.0
+    # the extra safety room we plan for along walls (as a percentage of car_width/2)
+    SAFETY_PERCENTAGE = 300.
 
+    def preprocess_lidar(self, ranges):
+        """ Any preprocessing of the LiDAR data can be done in this function.
+            Possible Improvements: smoothing of outliers in the data and placing
+            a cap on the maximum distance a point can be.
+        """
+        # remove quadrant of LiDAR directly behind us
+        eighth = int(len(ranges)/8)
+        return np.array(ranges[eighth:-eighth])
+    
+     
+    def get_differences(self, ranges):
+        """ Gets the absolute difference between adjacent elements in
+            in the LiDAR data and returns them in an array.
+            Possible Improvements: replace for loop with numpy array arithmetic
+        """
+        differences = [0.] # set first element to 0
+        for i in range(1, len(ranges)):
+            differences.append(abs(ranges[i]-ranges[i-1]))
+        return differences
+    
+    def get_disparities(self, differences, threshold):
+        """ Gets the indexes of the LiDAR points that were greatly
+            different to their adjacent point.
+            Possible Improvements: replace for loop with numpy array arithmetic
+        """
+        disparities = []
+        for index, difference in enumerate(differences):
+            if difference > threshold:
+                disparities.append(index)
+        return disparities
+
+    def get_num_points_to_cover(self, dist, width):
+        """ Returns the number of LiDAR points that correspond to a width at
+            a given distance.
+            We calculate the angle that would span the width at this distance,
+            then convert this angle to the number of LiDAR points that
+            span this angle.
+            Current math for angle:
+                sin(angle/2) = (w/2)/d) = w/2d
+                angle/2 = sininv(w/2d)
+                angle = 2sininv(w/2d)
+                where w is the width to cover, and d is the distance to the close
+                point.
+            Possible Improvements: use a different method to calculate the angle
+        """
+        angle = 2*np.arcsin(width/(2*dist))
+        num_points = int(np.ceil(angle / self.radians_per_point))
+        return num_points
+
+    def cover_points(self, num_points, start_idx, cover_right, ranges):
+        """ 'covers' a number of LiDAR points with the distance of a closer
+            LiDAR point, to avoid us crashing with the corner of the car.
+            num_points: the number of points to cover
+            start_idx: the LiDAR point we are using as our distance
+            cover_right: True/False, decides whether we cover the points to
+                         right or to the left of start_idx
+            ranges: the LiDAR points
+
+            Possible improvements: reduce this function to fewer lines
+        """
+        new_dist = ranges[start_idx]
+        if cover_right:
+            for i in range(num_points):
+                next_idx = start_idx+1+i
+                if next_idx >= len(ranges): break
+                if ranges[next_idx] > new_dist:
+                    ranges[next_idx] = new_dist
+        else:
+            for i in range(num_points):
+                next_idx = start_idx-1-i
+                if next_idx < 0: break
+                if ranges[next_idx] > new_dist:
+                    ranges[next_idx] = new_dist
+        return ranges
+
+    def extend_disparities(self, disparities, ranges, car_width, extra_pct):
+        """ For each pair of points we have decided have a large difference
+            between them, we choose which side to cover (the opposite to
+            the closer point), call the cover function, and return the
+            resultant covered array.
+            Possible Improvements: reduce to fewer lines
+        """
+        width_to_cover = (car_width/2) * (1+extra_pct/100)
+        for index in disparities:
+            first_idx = index-1
+            points = ranges[first_idx:first_idx+2]
+            close_idx = first_idx+np.argmin(points)
+            far_idx = first_idx+np.argmax(points)
+            close_dist = ranges[close_idx]
+            num_points_to_cover = self.get_num_points_to_cover(close_dist,
+                    width_to_cover)
+            cover_right = close_idx < far_idx
+            ranges = self.cover_points(num_points_to_cover, close_idx,
+                cover_right, ranges)
+        return ranges
+            
+    def get_steering_angle(self, range_index, range_len,ranges):
+        """ Calculate the angle that corresponds to a given LiDAR point and
+            process it into a steering angle.
+            Possible improvements: smoothing of aggressive steering angles
+        """
+
+        # if(ranges.argmin()>35):
+        #     steering_angle = 0
+        # else:
+        if range_index < 100 or range_index>980:
+            lidar_angle = (range_index - (range_len/2)) * self.radians_per_point
+            print(lidar_angle)
+            steering_angle = np.clip(lidar_angle, np.radians(-90), np.radians(90))*0.8
+        elif (ranges[int(len(ranges)/2)] < 5):
+            lidar_angle = (range_index - (range_len/2)) * self.radians_per_point*0.6
+            # print(lidar_angle)
+            steering_angle = np.clip(lidar_angle, np.radians(-90), np.radians(90))
+        elif (ranges[int(len(ranges)/2)] < 10):
+            lidar_angle = (range_index - (range_len/2)) * self.radians_per_point*0.3
+            # print(lidar_angle)
+            steering_angle = np.clip(lidar_angle, np.radians(-90), np.radians(90))
+        else:
+            lidar_angle = (range_index - (range_len/2)) * self.radians_per_point *0.2
+            # print(lidar_angle)
+            steering_angle = np.clip(lidar_angle, np.radians(-90), np.radians(90))
+
+        return steering_angle
+    def get_speed(self, ranges, max_disp,min_disp,proc_ranges,speed):
+        # speed = 12
+        # if (ranges[max_disp] - ranges[min_disp]) < 0.5:
+        #     speed = speed * 0.2
+        # if max_disp < 100 or max_disp>980:
+        #     speed = speed * 0.2
+        # if ranges[max_disp] < 3:
+        #     speed = speed *0.2
+        if (ranges[int(len(ranges)/2)] < 6) and (speed > 3) and (ranges[int(len(ranges)/2)+2] < 6) and (ranges[int(len(ranges)/2)-2] < 6):
+            speed = speed * 0.5
+        elif (ranges[int(len(ranges)/2)] < 10) and (speed > 9):
+            speed = speed * 0.7
+        elif (ranges[int(len(ranges)/2)] < 12) and (speed > 10):
+            speed = 10
+        else:
+            if speed < 8:
+                speed = speed * 1.1
+            if speed < 10:
+                speed = speed * 1.5
+            else:
+                speed = self.MAX_SPEED
+
+        return speed
     def process_lidar(self, ranges):
-        speed = 5.0
-        steering_angle = 0.0
+        """ Run the disparity extender algorithm!
+            Possible improvements: varying the speed based on the
+            steering angle or the distance to the farthest point.
+        """
+        self.radians_per_point = (2*np.pi)/len(ranges)
+        proc_ranges = self.preprocess_lidar(ranges)
+        differences = self.get_differences(proc_ranges)
+        disparities = self.get_disparities(differences, self.DIFFERENCE_THRESHOLD)
+        proc_ranges = self.extend_disparities(disparities, proc_ranges,
+                self.CAR_WIDTH, self.SAFETY_PERCENTAGE)
+        steering_angle = self.get_steering_angle(proc_ranges.argmax(),
+                len(proc_ranges),ranges)
+        
+        speed = self.get_speed(ranges,proc_ranges.argmax(),proc_ranges.argmin(),proc_ranges,self.SPEED)
+        self.SPEED = speed
+        print(self.SPEED)
+        # if starp_corner
         return speed, steering_angle
 #------------------------------------------------------------------------
 #------------------------------------------------------------------------
